702077db6e770c7ed1cb9b4b574e22a9dbc89e4d
Lylak Ivan <lylakivanmate@gmail.com>
2023-12-13 13:33:53 +0200

    create models: DishType, Dish & Cook. Add AUTH_USER_MODEL

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from kitchen_service import settings
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        verbose_name = "dish type"
+        verbose_name_plural = "dish types"
+        ordering = ('name',)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(blank=False, validators=[MinValueValidator(1), MaxValueValidator(40)])
+
+    class Meta:
+        ordering = ["-years_of_experience"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.00)])
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "dishes"
+        ordering = ("name",)
+
